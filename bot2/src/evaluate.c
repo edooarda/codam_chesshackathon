e46e5687e82dbbb0b9cb52973a8af17f7f403022
Merel <mvan-ess@codam.student.nl>
2025-03-13 15:00:15 +0100

    we won

--- bot2/src/evaluate.c
+++ bot2/src/evaluate.c
@@ -6,6 +6,162 @@
 
 static const int piece_value[6] = { 100, 300, 300, 500, 900, 10000 };
 
+static const int pawn_table_w[64] = { 0,  0,  0,  0,  0,  0,  0,  0,
+										50, 50, 50, 50, 50, 50, 50, 50,
+										10, 10, 20, 30, 30, 20, 10, 10,
+										5,  5, 10, 25, 25, 10,  5,  5,
+										0,  0,  0, 20, 20,  0,  0,  0,
+										5, -5,-10,  0,  0,-10, -5,  5,
+										5, 10, 10,-20,-20, 10, 10,  5,
+										0,  0,  0,  0,  0,  0,  0,  0
+};
+
+static const int king_table_w[64] = { -30,-40,-40,-50,-50,-40,-40,-30,
+												-30,-40,-40,-50,-50,-40,-40,-30,
+												-30,-40,-40,-50,-50,-40,-40,-30,
+												-30,-40,-40,-50,-50,-40,-40,-30,
+												-20,-30,-30,-40,-40,-30,-30,-20,
+												-10,-20,-20,-20,-20,-20,-20,-10,
+												20, 20,  0,  0,  0,  0, 20, 20,
+												20, 30, 10,  0,  0, 10, 30, 20
+										};
+static const int king_table_endgame_w[64] = { -50,-40,-30,-20,-20,-30,-40,-50,
+												-30,-20,-10,  0,  0,-10,-20,-30,
+												-30,-10, 20, 30, 30, 20,-10,-30,
+												-30,-10, 30, 40, 40, 30,-10,-30,
+												-30,-10, 30, 40, 40, 30,-10,-30,
+												-30,-10, 20, 30, 30, 20,-10,-30,
+												-30,-30,  0,  0,  0,  0,-30,-30,
+												-50,-30,-30,-30,-30,-30,-30,-50
+												};
+static const int queen_table_w[64] = { -20,-10,-10, -5, -5,-10,-10,-20,
+											-10,  0,  0,  0,  0,  0,  0,-10,
+											-10,  0,  5,  5,  5,  5,  0,-10,
+											-5,  0,  5,  5,  5,  5,  0, -5,
+											0,  0,  5,  5,  5,  5,  0, -5,
+											-10,  5,  5,  5,  5,  5,  0,-10,
+											-10,  0,  5,  0,  0,  0,  0,-10,
+											-20,-10,-10, -5, -5,-10,-10,-20
+										};
+static const int rook_table_w[64] = { 0,  0,  0,  0,  0,  0,  0,  0,
+										5, 10, 10, 10, 10, 10, 10,  5,
+										-5,  0,  0,  0,  0,  0,  0, -5,
+										-5,  0,  0,  0,  0,  0,  0, -5,
+										-5,  0,  0,  0,  0,  0,  0, -5,
+										-5,  0,  0,  0,  0,  0,  0, -5,
+										-5,  0,  0,  0,  0,  0,  0, -5,
+										0,  0,  0,  5,  5,  0,  0,  0
+									};
+static const int bishop_table_w[64] = { -20,-10,-10,-10,-10,-10,-10,-20,
+										-10,  0,  0,  0,  0,  0,  0,-10,
+										-10,  0,  5, 10, 10,  5,  0,-10,
+										-10,  5,  5, 10, 10,  5,  5,-10,
+										-10,  0, 10, 10, 10, 10,  0,-10,
+										-10, 10, 10, 10, 10, 10, 10,-10,
+										-10,  5,  0,  0,  0,  0,  5,-10,
+										-20,-10,-10,-10,-10,-10,-10,-20
+									};
+static const int knight_table_w[64] = {-50,-40,-30,-30,-30,-30,-40,-50,
+										-40,-20,  0,  0,  0,  0,-20,-40,
+										-30,  0, 10, 15, 15, 10,  0,-30,
+										-30,  5, 15, 20, 20, 15,  5,-30,
+										-30,  0, 15, 20, 20, 15,  0,-30,
+										-30,  5, 10, 15, 15, 10,  5,-30,
+										-40,-20,  0,  5,  5,  0,-20,-40,
+										-50,-40,-30,-30,-30,-30,-40,-50
+									};
+
+int tablescore(int piece, int color, int square, int endgame) {
+	int new_square;
+	if (color == WHITE)
+		new_square = 63;
+	else
+		new_square = 63 - square;
+	if (piece == PAWN)
+		return (pawn_table_w[new_square]); /*flip to black*/
+	else if (piece == KNIGHT)
+		return (knight_table_w[new_square]);
+	else if (piece == QUEEN)
+		return (queen_table_w[new_square]);
+	else if (piece == ROOK)
+		return (rook_table_w[new_square]);
+	else if (piece == KING && endgame)
+		return (king_table_endgame_w[new_square]);
+	else if (piece == KING)
+		return (king_table_w[new_square]);
+	else if (piece == BISHOP)
+		return (bishop_table_w[new_square]);
+	return (0);
+}
+
+int	is_possible_move(struct move move, const struct position *pos) {
+	int color;
+	int	i;
+	int	reach;
+	const int	knight_moves[8] = {-17, -15, -10, -6, 6, 10, 15, 17};
+	const int	bishop_directions[4] = { -9, -7, 7, 9 };
+	const int	rook_directions[4] = { -8, 8, -1, 1 };
+	const int	king_moves[] = {-9, -8, -7, -1, 1, 7, 8, 9};
+	
+	if (move.promotion_type == PAWN) {
+		if (COLOR(pos->board[move.from_square]) == WHITE)
+			color = 1;
+		else
+			color = -1;
+		if (move.from_square + (7 * color) == move.to_square ||
+			move.from_square + (9 * color) == move.to_square)
+			return (1);
+	}
+	if (move.promotion_type == KNIGHT) {
+		for (i = 0; i < 8; i++)
+		{
+			if (move.from_square + knight_moves[i] == move.to_square)
+				return (1);
+		}
+		return (0);
+	}
+	if (move.promotion_type == BISHOP || move.promotion_type == QUEEN) {
+		for (i = 0; i < 4; i++)
+		{
+			reach = move.from_square;
+			while (1)
+			{
+				reach += bishop_directions[i];
+				if (reach < 0 || reach >= 64 || (FILE(reach) == 0 && FILE(move.from_square) == 7) || (FILE(reach) == 7 && FILE(move.from_square) == 0))
+					break;
+				if (reach == move.to_square)
+					return (1);
+			}
+		}
+		if (move.promotion_type == BISHOP)
+			return (0);
+	}
+	if (move.promotion_type == ROOK || move.promotion_type == QUEEN) {
+		for (i = 0; i < 4; i++)
+		{
+			reach = move.from_square;
+			while (1)
+			{
+				reach += rook_directions[i];
+				if (reach < 0 || reach >= 64)
+					break;
+				if (reach == move.to_square)
+					return (1);
+			}
+		}
+		return (0);
+	}
+	if (move.promotion_type == KING) {
+		for (i = 0; i < 8; i++)
+		{
+			if (move.from_square + king_moves[i] == move.to_square)
+				return (1);
+		}
+		return (0);
+	}
+	return (0);
+}
+
 /* Losing a piece is bad! Winning is good*/
 void	attack_score(int *score_total, const struct position *pos, int square) {
 	struct move		move;
@@ -20,17 +176,16 @@ void	attack_score(int *score_total, const struct position *pos, int square) {
 			move.from_square = i;
 			move.to_square = square;
 			move.promotion_type = TYPE(piece);
-			if (is_legal(pos, move))
+			
+			if (is_possible_move(move, pos))
 			{
 				piece = pos->board[square];
 				(*score_total) -= piece_value[TYPE(piece)];
-				piece = pos->board[square];
-				if (piece != NO_PIECE)
-					(*score_total) -= piece_value[TYPE(piece)];
 			}
 		}
-    }
+	}
 }
+
 
 /* How more steps a pawn have made how higher the value */
 int	pawn_score(int rank, int piece) {
@@ -53,7 +208,7 @@ int	king_middle_field_score(int square) {
 }
 
 /*Check if the queens are still in play*/
-bool queens_still_in_play(const struct position *pos) {
+int queens_still_in_play(const struct position *pos) {
 	int square;
 	int piece;
 	int count = 2;
@@ -65,24 +220,27 @@ bool queens_still_in_play(const struct position *pos) {
 		}
 	}
 	if (count == 0)
-		return (false);
-	return (true);
+		return (0);
+	return (1);
 }
 
 int evaluate(const struct position *pos) {
 	int score[2] = { 0, 0 };
 	int square;
 	int piece;
+	int	no_queens = 0;
 
-	if (!queens_still_in_play(pos)){
-		//Change king score board.
-	}
+	if (!queens_still_in_play(pos))
+		no_queens = 1;
 	for (square = 0; square < 64; square++) {
 		piece = pos->board[square];
 		
 		if (piece != NO_PIECE)
 		{
 			score[COLOR(piece)] += piece_value[TYPE(piece)];
+			
+			score[COLOR(piece)] += tablescore(TYPE(piece), COLOR(piece), square, no_queens);
+
 			if (TYPE(piece) == PAWN)
 				score[COLOR(piece)] += pawn_score(RANK(square), piece);
 
--- bot2/src/uci.c
+++ bot2/src/uci.c
@@ -53,6 +53,8 @@ static char *get_token(char *string, char *store) {
 	return NULL;
 }
 
+static char last_move_other_side[] = "0000";
+
 static void uci_position(struct position *pos, char *token, char *store) {
 	token = get_token(token, store);
 
@@ -80,35 +82,44 @@ static void uci_position(struct position *pos, char *token, char *store) {
 			struct move move;
 
 			if (parse_move(&move, token) == SUCCESS) {
+				strcpy(last_move_other_side, token);
 				do_move(pos, move);
 			}
 		}
 	}
 }
 
-static int moves_total_game = -1;
-/*char defence_e2e4_Black[4][4] = {"b8c6", "e7e6", "d7d5", "c7c6"};*/
+static int moves_total_game = 0;
+struct opening {
+	char moves[3][6];
+	int move_count;
+	int is_white;
+};
 
-int openingsbook(const struct position *pos) {
-	static char *move = NULL;
+static struct opening openings[5] = {
+    { { "e2e4", "g1f3", "d2d4" }, 3, 1 },
+    { { "d2d4", "c2c4", "g1f3" }, 3, 1 },
+    { { "c7c5", "d7d6", "g8f6" }, 3, 0 },
+    { { "e7e5", "g8f6", "d7d6" }, 3, 0 },
+    { { "d7d5", "c7c6", "e7e6" }, 3, 0 } 
+};
 
-	if (pos->side_to_move == WHITE)
-	{
-		if (moves_total_game == 0)
-			move = "e2e4";
-		else if (moves_total_game == 1)
-			move = "g1f3";
-	}
-	else
-	{
-		if (moves_total_game == 0)
-			move = "d7d5";
-	}
-	
-	if (move == NULL)
-		return (0);
-	printf("bestmove %s\n", move);
-	return (1);
+int openingsbook(const struct position *pos, int moves) {
+	static char		move[6];
+	int				i;
+
+    for (i = 0; i < 3; i++) {
+        if ((pos->side_to_move == WHITE && openings[i].is_white) ||
+            (pos->side_to_move == BLACK && !openings[i].is_white)) {
+            
+            if (moves < openings[i].move_count) {
+                strcpy(move, openings[i].moves[moves]);
+                printf("bestmove %s\n", move);
+                return (1);
+            }
+        }
+    }
+	return (0);
 }
 
 /*
@@ -155,11 +166,11 @@ static void uci_go(const struct position *pos, char *token, char *store) {
 			break;
 		}
 	}
-	moves_total_game++;
-	if (moves_total_game < 1)
+
+	if (openingsbook(pos, moves_total_game))
 	{
-		if (openingsbook(pos))
-			return ;
+		moves_total_game++; 
+		return ;
 	}
 	move = search(&info);
 
